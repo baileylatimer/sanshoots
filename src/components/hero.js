a748713c2a8e9b4833f968a8afe01d15a3ba06e7
Bailey Latimer <baileyl@beautyindustrygroup.com>
2024-01-23 13:00:38 -0800

    added reel to hero

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react";
 import BackgroundVideo from "./bg-video"
+import ReelBlock from "./reel-block"
+import Logo from "./logo"
 import Video from "../videos/hero-vid.mp4"
 
 import { gsap } from "gsap";
@@ -9,23 +11,26 @@ const Hero = props => (
   
 <div class="hero">
 
-<svg className="hero-reel mx-24 mt-36" width="1387" height="70" viewBox="0 0 1387 70" fill="none" xmlns="http://www.w3.org/2000/svg">
-<rect width="39.1366" height="69.4875" rx="10.3832" fill="#232323"/>
-<rect x="122.533" width="39.1366" height="69.4875" rx="10.3832" fill="#232323"/>
-<rect x="245.066" width="39.1366" height="69.4875" rx="10.3832" fill="#232323"/>
-<rect x="367.599" width="39.1366" height="69.4875" rx="10.3832" fill="#232323"/>
-<rect x="490.132" width="39.1366" height="69.4875" rx="10.3832" fill="#232323"/>
-<rect x="612.665" width="39.1366" height="69.4875" rx="10.3832" fill="#232323"/>
-<rect x="735.198" width="39.1366" height="69.4875" rx="10.3832" fill="#232323"/>
-<rect x="857.731" width="39.1366" height="69.4875" rx="10.3832" fill="#232323"/>
-<rect x="980.264" width="39.1366" height="69.4875" rx="10.3832" fill="#232323"/>
-<rect x="1102.8" width="39.1366" height="69.4875" rx="10.3832" fill="#232323"/>
-<rect x="1225.33" width="39.1366" height="69.4875" rx="10.3832" fill="#232323"/>
-<rect x="1347.86" width="39.1366" height="69.4875" rx="10.3832" fill="#232323"/>
-</svg>
+<div className="flex justify-between hero-reel mx-24 mt-36">
+<ReelBlock />
+<ReelBlock />
+<ReelBlock />
+<ReelBlock />
+<ReelBlock />
+<ReelBlock />
+<ReelBlock />
+<ReelBlock />
+<ReelBlock />
+<ReelBlock />
+<ReelBlock />
+<ReelBlock />
+</div>
 
-<div className="overlay-bottom"></div>
-<h1 className="text-xxl mt-36  ro">SANSHOOTS</h1>
+
+<div className=" ">
+<h1 className="text-xxl text-center">SANSHOOTS</h1>
+</div>
+
 <p className="hero-desc text-white ">Rocio Colomer Jorda has 15 years of dance experience. With influences from her home country of spain, she now teaches in San Diego, CA.</p>
 </div>
  
---
+++ src/components/reel-block.js
@@ -0,0 +1,17 @@
+
+
+
+import React from "react"
+
+const ReelBlock = () => {
+  return (
+
+<svg className="reel-block" width="40" height="70" viewBox="0 0 40 70" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect x="0.731201" width="39.1366" height="69.4875" rx="10.3832" fill="#232323"/>
+</svg>
+
+  );
+};
+
+
+export default ReelBlock;
