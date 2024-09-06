13ff6f2420c05c296e036b802f7909c49c04334f
Bailey Latimer <baileyl@beautyindustrygroup.com>
2024-09-06 14:01:57 -0700

    updated home projects content

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,11 +13,20 @@ import PostProduction from "../videos/post-production.mp4"
 
 // Import video files directly
 import Elyanna from "../videos/elyanna-2.mp4";
-import NourArdakani from "../videos/production.mp4";
+import YungBans from "../videos/yung-bans.mp4";
+import NourArdakani from "../videos/nour-ardakani.mp4";
 import FrancisMercier from "../videos/francis-mercier.mp4";
-import Paliroots from "../videos/production.mp4";
-import Bluemoon from "../videos/san-eye.mp4";
-import Axe from "../videos/san-sand-1.mp4";
+import Paliroots from "../videos/paliroots.mp4";
+import Bluemoon from "../videos/bluemoon.mp4";
+import Axe from "../videos/axe.mp4";
+import Gillette from "../videos/gillette.mp4";
+import Celsius from "../videos/celsius.mp4";
+import HighEndExotics from "../videos/highendexotics.mp4";
+import Thg from "../videos/thehartfordgroup.mp4";
+import Umr from "../videos/umr-relief.mp4";
+import IntuitiveFoundation from "../videos/intuitive-foundation.mp4";
+import EminentCollaborations from "../videos/eminent-collaborations.mp4";
+import ColombiaAirbnb from "../videos/el-silencio-glamping.mp4";
 
 
 // Sample data for the slider
@@ -25,39 +34,39 @@ const slidesData = [
   {
     videoSrc: Elyanna,
     title: "Elyanna",
-    tag: "Music",
+    tag: "Event",
     linkTo: "/work/elyanna"
+  },
+  {
+    videoSrc: YungBans,
+    title: "Yung Bans",
+    tag: "Music Video",
+    linkTo: "/work/yung-bans"
   },
   {
     videoSrc: FrancisMercier,
     title: "Francis Mercier",
-    tag: "Music",
-    linkTo: "/second-video"
-  },
-  {
-    videoSrc: NourArdakani,
-    title: "Nour Ardakani",
-    tag: "Music",
-    linkTo: "/second-video"
+    tag: "Event",
+    linkTo: "/work/francis-mercier"
   },
 
   {
+    videoSrc: NourArdakani,
+    title: "Nour Ardakani",
+    tag: "Music Video",
+    linkTo: "/work/nour-ardakani"
+  },
+  {
     videoSrc: Paliroots,
     title: "Paliroots",
-    tag: "Fashion",
-    linkTo: "/second-video"
+    tag: "Apparel",
+    linkTo: "/work/paliroots"
   },
   {
     videoSrc: Bluemoon,
     title: "Bluemoon",
     tag: "Jewelry",
-    linkTo: "/second-video"
-  },
-  {
-    videoSrc: Axe,
-    title: "Axe",
-    tag: "Beauty",
-    linkTo: "/second-video"
+    linkTo: "/work/bluemoon"
   }
 ];
 
