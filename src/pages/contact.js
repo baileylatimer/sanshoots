3219e9056dce5d722ef2b9d2b564d3312d29c17e
Bailey Latimer <baileyl@beautyindustrygroup.com>
2024-09-06 21:14:52 -0700

    added typeform

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,24 +1,54 @@
-import React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
-
+import React, { useEffect, useRef } from 'react';
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-
 import PageHeader from "../components/page-header"
+import { createWidget } from '@typeform/embed';
 
+const typeformURL = 'https://form.typeform.com/to/LsunBzR3';
 
+const TypeformEmbed = () => {
+  const typeformRef = useRef(null);
 
-const ContactPage = () => (
-  <Layout>
-    <SEO title="Contact" />
-    <PageHeader title="Contact"/>
-<div className="contact flex flex-col justify-center items-center py-24 lg:py-40">
-  <a tel="+1 (858) 776-8431">+1 (858) 776-8431</a>
-  <a mailto="hello@rocio.dance">hello@rocio.dance</a>
-</div>
+  useEffect(() => {
+    if (typeformRef.current) {
+      createWidget(typeformURL, {
+        container: typeformRef.current,
+        width: '100%',
+        height: '100vh',
+      });
+    }
+  }, []);
 
-  </Layout>
-)
+  return (
+    <Layout>
+      <SEO title="Contact" />
+      <PageHeader title="Contact"/>
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100vw',
+        height: '60vh',
+        backgroundColor: '#f8f8f8',
+        position: 'relative',
+      }}>
+        {/* Typeform container */}
+        <div 
+          ref={typeformRef} 
+          style={{ 
+            width: '100%',
+            height: '100%',
+            maxWidth: '100%',
+            maxHeight: '100%',
+            position: 'absolute',
+            top: '0',
+            left: '0',
+            border: 'none'
+          }}
+        ></div>
+      </div>
+    </Layout>
+  );
+};
 
-export default ContactPage
+export default TypeformEmbed;
