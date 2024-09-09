cc9ec6f01495d03fffd67bb73e378a2d9cbee669
Bailey Latimer <baileyl@beautyindustrygroup.com>
2024-09-09 16:40:34 -0700

    updated video slider for ios support

--- src/components/video-slide.js
+++ src/components/video-slide.js
@@ -1,33 +1,98 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { Link } from "gatsby";
 
-const VideoSlide = ({ videoSrc, title, tag, linkTo }) => {
+const VideoSlide = ({ webmSrc, mp4Src, fallbackImageSrc, title, tag, linkTo }) => {
   const videoRef = useRef(null);
+  const [isVideoLoaded, setIsVideoLoaded] = useState(false);
+  const [hasVideoError, setHasVideoError] = useState(false);
+  const [isIOS, setIsIOS] = useState(false);
+
+  useEffect(() => {
+    // Check if the device is running iOS
+    const ua = window.navigator.userAgent;
+    const iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
+    setIsIOS(iOS);
+
+    if (videoRef.current) {
+      videoRef.current.defaultMuted = true;
+      videoRef.current.muted = true;
+    }
+
+    const playVideoOnTouch = () => {
+      if (videoRef.current) {
+        videoRef.current.play().catch(error => {
+          console.error("Autoplay failed:", error);
+        });
+      }
+    };
+
+    document.addEventListener('touchstart', playVideoOnTouch);
+
+    return () => {
+      document.removeEventListener('touchstart', playVideoOnTouch);
+    };
+  }, []);
+
+  const handleVideoLoaded = () => {
+    console.log("Video loaded successfully");
+    setIsVideoLoaded(true);
+  };
+
+  const handleVideoError = (e) => {
+    console.error("Video loading error:", e);
+    setHasVideoError(true);
+  };
 
   const handleMouseEnter = () => {
-    videoRef.current.play();
+    if (videoRef.current && !isIOS) {
+      videoRef.current.play();
+    }
   };
 
   const handleMouseLeave = () => {
-    videoRef.current.pause();
+    if (videoRef.current && !isIOS) {
+      videoRef.current.pause();
+    }
   };
 
   return (
     <Link to={linkTo} className="video-slide pl-4">
       <div className="video-container">
+        <img 
+          src={fallbackImageSrc} 
+          alt={title}
+          className={`fallback-image ${isVideoLoaded && !hasVideoError ? 'hidden' : ''}`}
+        />
         <video
           ref={videoRef}
-          src={videoSrc}
+          className={`video-element ${isVideoLoaded && !hasVideoError ? '' : 'hidden'}`}
           loop
           muted
+          playsInline
+          webkit-playsinline="true"
+          onLoadedData={handleVideoLoaded}
+          onError={handleVideoError}
           onMouseEnter={handleMouseEnter}
           onMouseLeave={handleMouseLeave}
-        />
+        >
+          {isIOS ? (
+            <>
+              <source src={mp4Src} type="video/mp4" />
+              <source src={webmSrc} type="video/webm" />
+            </>
+          ) : (
+            <>
+              <source src={webmSrc} type="video/webm" />
+              <source src={mp4Src} type="video/mp4" />
+            </>
+          )}
+          Your browser does not support the video tag.
+        </video>
         <div className="overlay"></div>
         <div className="video-info flex justify-between items-center w-full">
           <h3 className="color-bg">{title}</h3>
           <div className="tag">
-          <p className="color-bg">{tag}</p>
+            <p className="color-bg">{tag}</p>
           </div>
         </div>
       </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -13,32 +13,32 @@ import PostProduction from "../videos/post-production.mp4"
 
 // Import video files directly
 import Elyanna from "../videos/elyanna-2.mp4";
+import Elyanna2 from "../videos/elyanna-2.webm";
+import Elyanna3 from "../images/elyanna.jpg";
 import YungBans from "../videos/yung-bans.mp4";
+import YungBans2 from "../videos/yung-bans.webm";
+import YungBans3 from "../images/yung-bans.jpg";
 import NourArdakani from "../videos/nour-ardakani.mp4";
 import FrancisMercier from "../videos/francis-mercier.mp4";
 import Paliroots from "../videos/paliroots.mp4";
 import Bluemoon from "../videos/bluemoon.mp4";
-import Axe from "../videos/axe.mp4";
-import Gillette from "../videos/gillette.mp4";
-import Celsius from "../videos/celsius.mp4";
-import HighEndExotics from "../videos/highendexotics.mp4";
-import Thg from "../videos/thehartfordgroup.mp4";
-import Umr from "../videos/umr-relief.mp4";
-import IntuitiveFoundation from "../videos/intuitive-foundation.mp4";
-import EminentCollaborations from "../videos/eminent-collaborations.mp4";
-import ColombiaAirbnb from "../videos/el-silencio-glamping.mp4";
+
 
 
 // Sample data for the slider
 const slidesData = [
   {
-    videoSrc: Elyanna,
+    mp4Src: Elyanna,
+    webmSrc: Elyanna2,
+    fallbackImageSrc: Elyanna3,
     title: "Elyanna",
     tag: "Event",
     linkTo: "/showreel/elyanna"
   },
   {
-    videoSrc: YungBans,
+    mp4Src: YungBans,
+    webmSrc: YungBans2,
+    fallbackImageSrc: YungBans3,
     title: "Yung Bans",
     tag: "Music Video",
     linkTo: "/showreel/yung-bans"
